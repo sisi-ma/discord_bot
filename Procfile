0b5abb36f608b18ed1558c2e4372e08c13e79ec0
sisi-ma <perfectmaid0702@gmail.com>
2020-11-28 17:20:00 +0900

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+discordbot: python tools.py
---
+++ tools.py
@@ -0,0 +1,95 @@
+import discord
+import random
+from googleapiclient.discovery import build
+from discord.ext import commands
+import requests
+from bs4 import BeautifulSoup
+import const
+
+bot = commands.Bot(command_prefix="!",help_command=None)
+
+# 起動時に動作する処理
+@bot.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('login.')
+
+# メッセージ受信時に動作する処理
+@bot.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    elif message.content == '!dice' or message.content == '!d' or message.content == '!1d6':
+        await message.channel.send(random.randint(1, 6))
+    elif message.content == '!2d6':
+        await message.channel.send(str(random.randint(1, 6)) + ', ' + str(random.randint(1, 6)))
+    elif message.content == '!3d6':
+        await message.channel.send(str(random.randint(1, 6)) + ', ' + str(random.randint(1, 6)) + ', ' + str(random.randint(1, 6)))
+    elif message.content == '!d10' or message.content == '!1d10':
+        await message.channel.send(random.randint(1, 10))
+    elif message.content == '!2d10':
+        await message.channel.send(str(random.randint(1, 10)) + ', ' + str(random.randint(1, 10)))
+    elif message.content == '!3d10':
+        await message.channel.send(str(random.randint(1, 100)) + ', ' + str(random.randint(1, 100)) + ', ' + str(random.randint(1, 100)))
+    elif message.content == '!d100' or message.content == '!1d100':
+        await message.channel.send(random.randint(1, 100))
+    elif message.content == '!2d100':
+        await message.channel.send(str(random.randint(1, 100)) + ', ' + str(random.randint(1, 100)))
+    elif message.content == '!3d100':
+        await message.channel.send(str(random.randint(1, 100)) + ', ' + str(random.randint(1, 100)) + ', ' + str(random.randint(1, 100)))
+    elif random.randint(1, 200) == 200:
+        await message.channel.send('ワイトもそう思います。')
+
+    await bot.process_commands(message)
+
+@bot.command(aliases=['h'])
+async def help(ctx):
+    await ctx.send((
+        '```\n'
+        '!help:    コマンド一覧を表示します（今表示してるこれ）\n'
+        '          省略形として「!h」でも表示できます。\n'
+        '\n'
+        '!dice:    ダイスを振ります。6面、10面、100面ダイスがあり、「!3d6」のように指定することで最大3つまで同時に投げることができます。\n'
+        '          省略形として「!d」で6面ダイスを振れます。\n'
+        '\n'
+        '!ygoggr:  遊戯王カードWiki内を検索し、効果テキスト欄を持つページを1件先頭から探しタイトルと効果テキストを返します。\n'
+        '          「!ygoggr ホープ」のように指定してください。省略形として「!y ホープ」でも指定できます。\n'
+        '          「!y ドラグーン　禁止」のようにスペース区切りで複数キーワードも指定できます。\n'
+        '\n'
+        '!passive: 天則、スマブラ、遊戯王の中からランダムに一つ返します。\n'
+        '          省略形として「!p」「!pa」でもOKです。\n'
+        '```'
+    ))
+
+@bot.command(aliases=['y', 'ygo'])
+async def ygoggr(ctx, *search_words):
+    results = getYGOSearchResponse(' '.join(search_words))
+
+    for result in results:
+        url = requests.get(result['link'])
+        soup = BeautifulSoup(url.content, 'html.parser').find_all('pre')
+        if soup != []:
+            # output = '**'+str(result['title'])+'**\n '+str(result['link']+' \n```\n')+str(soup[0])[5:-6]+'\n```' # URL込み
+            output = '**'+str(result['title'])+'**\n```\n'+str(soup[0])[5:-6]+'\n```'
+            break
+
+    print(output)
+    await ctx.send(output)
+
+def getYGOSearchResponse(search_word):
+    service = build("customsearch", "v1", developerKey=const.GOOGLE_API_KEY)
+    response = service.cse().list(
+        q=search_word,
+        cx=const.CUSTOM_SEARCH_ENGINE_ID,
+        lr='lang_ja',
+        num=10,
+    ).execute()
+    return response['items']
+
+@bot.command(aliases=['p', 'pa'])
+async def passive(ctx):
+    await ctx.send(random.choice(('天則', 'スマブラ', '遊戯王')))
+
+# Botの起動とDiscordサーバーへの接続
+bot.run(const.DISCORD_TOKEN)
