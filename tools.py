39240632e29cd91f274780b40c9b12f7e678bd7d
sisi-ma <perfectmaid0702@gmail.com>
2021-03-02 23:49:07 +0900

    fix SSLErrorが発生していたため、しぶしぶSSL認証を回避

--- tools.py
+++ tools.py
@@ -7,15 +7,15 @@ from googleapiclient.discovery import build
 from bs4 import BeautifulSoup
 
 # const.py用
-# import const
-# GOOGLE_API_KEY          = const.GOOGLE_API_KEY
-# CUSTOM_SEARCH_ENGINE_ID = const.CUSTOM_SEARCH_ENGINE_ID
-# DISCORD_TOKEN           = const.DISCORD_TOKEN
+import const
+GOOGLE_API_KEY          = const.GOOGLE_API_KEY
+CUSTOM_SEARCH_ENGINE_ID = const.CUSTOM_SEARCH_ENGINE_ID
+DISCORD_TOKEN           = const.DISCORD_TOKEN
 
 # Heroku用
-GOOGLE_API_KEY          = os.environ['GOOGLE_API_KEY']
-CUSTOM_SEARCH_ENGINE_ID = os.environ['CUSTOM_SEARCH_ENGINE_ID']
-DISCORD_TOKEN           = os.environ['DISCORD_TOKEN']
+# GOOGLE_API_KEY          = os.environ['GOOGLE_API_KEY']
+# CUSTOM_SEARCH_ENGINE_ID = os.environ['CUSTOM_SEARCH_ENGINE_ID']
+# DISCORD_TOKEN           = os.environ['DISCORD_TOKEN']
 
 # ! + 関数名 のメッセージ送信でコマンド実行と定義
 bot = commands.Bot(command_prefix="!",help_command=None)
@@ -76,7 +76,7 @@ async def ygoggr(ctx, *search_words):
 
     # 検索結果から<pre>タグを持つページを1件引っ張ってきて文字列加工
     for result in results:
-        url = requests.get(result['link'])
+        url = requests.get(result['link'], verify=False)
         soup = BeautifulSoup(url.content, 'html.parser').find_all('pre')
         if soup != []:
             output = '**'+str(result['title'])+'**\n```\n'+str(soup[0])[5:-6]+'\n```'
