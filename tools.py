5960258f1a5c3b35dd988567eecab8f2f9a9cbe7
sisi-ma <perfectmaid0702@gmail.com>
2021-04-21 21:59:03 +0900

    update スプレッドシートからランダムに取ってくる機能追加

--- tools.py
+++ tools.py
@@ -7,15 +7,17 @@ from googleapiclient.discovery import build
 from bs4 import BeautifulSoup
 
 # const.py用
-# import const
-# GOOGLE_API_KEY          = const.GOOGLE_API_KEY
-# CUSTOM_SEARCH_ENGINE_ID = const.CUSTOM_SEARCH_ENGINE_ID
-# DISCORD_TOKEN           = const.DISCORD_TOKEN
+import const
+GOOGLE_API_KEY                 = const.GOOGLE_API_KEY
+CUSTOM_SEARCH_ENGINE_ID        = const.CUSTOM_SEARCH_ENGINE_ID
+DISCORD_TOKEN                  = const.DISCORD_TOKEN
+GOOGLE_APPLICATION_CREDENTIALS = const.GOOGLE_APPLICATION_CREDENTIALS
 
 # Heroku用
-GOOGLE_API_KEY          = os.environ['GOOGLE_API_KEY']
-CUSTOM_SEARCH_ENGINE_ID = os.environ['CUSTOM_SEARCH_ENGINE_ID']
-DISCORD_TOKEN           = os.environ['DISCORD_TOKEN']
+# GOOGLE_API_KEY                 = os.environ['GOOGLE_API_KEY']
+# CUSTOM_SEARCH_ENGINE_ID        = os.environ['CUSTOM_SEARCH_ENGINE_ID']
+# DISCORD_TOKEN                  = os.environ['DISCORD_TOKEN']
+# GOOGLE_APPLICATION_CREDENTIALS = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
 
 # ! + 関数名 のメッセージ送信でコマンド実行と定義
 bot = commands.Bot(command_prefix="!",help_command=None)
@@ -45,19 +47,24 @@ async def on_message(message):
 async def help(ctx):
     await ctx.send(
         '```\n'
-        '!help:    コマンド一覧を表示します（今表示してるこれ）\n'
-        '          省略形として「!h」でも表示できます。\n'
+        '!help:     コマンド一覧を表示します（今表示してるこれ）\n'
+        '           省略形として「!h」でも表示できます。\n'
         '\n'
-        '!dice:    ダイスを振ります。省略形として「!d」で6面ダイス、また「!1d100」で100面ダイスを振れます。\n'
+        '!dice:     ダイスを振ります。省略形として「!d」で6面ダイス、また「!1d100」で100面ダイスを振れます。\n'
         '\n'
-        '!ygoggr:  遊戯王カードWiki内の検索結果を表示します。\n'
-        '          効果テキスト欄を持つページを1件先頭から探し、タイトルと効果テキストを返します。\n'
-        '          「!ygoggr ホープ」のように指定してください。省略形として「!y ホープ」でもOKです。\n'
-        '          「!y ドラグーン　禁止」のようにスペース区切りで複数キーワードも指定できます。\n'
-        '          あとはものによりますが「!y レダメ」のように略称も案外いけます。\n'
+        '!ygoggr:   遊戯王カードWiki内の検索結果を表示します。\n'
+        '           効果テキスト欄を持つページを1件先頭から探し、タイトルと効果テキストを返します。\n'
+        '           「!ygoggr ホープ」のように指定してください。省略形として「!y ホープ」でもOKです。\n'
+        '           「!y ドラグーン　禁止」のようにスペース区切りで複数キーワードも指定できます。\n'
+        '           あとはものによりますが「!y レダメ」のように略称も案外いけます。\n'
         '\n'
-        '!passive: 天則、憑依華、遊戯王、モンハンの中からランダムに一つ返します。\n'
-        '          省略形として「!p」「!pa」でもOKです。\n'
+        '!passive:  天則、憑依華、遊戯王、モンハンの中からランダムに一つ返します。\n'
+        '           省略形として「!p」「!pa」でもOKです。\n'
+        '\n'
+        '!randlist: スプレッドシートの項目からランダムに一つ返します。\n'
+        '           https://docs.google.com/spreadsheets/d/1LZq1Pmqt_PRYTnkKz7ershaG2ILQTMTVFnGEAOsaYDQ/edit'
+        '           上記のSheet1、セルA1からA10までが対象です。'
+        '           省略形として「!r」「!rl」でもOKです。\n'
         '```'
     )
 
@@ -100,6 +107,25 @@ def getYGOSearchResponse(search_words):
 @bot.command(aliases=['p', 'pa'])
 async def passive(ctx):
     await ctx.send(random.choice(('天則', '憑依華', '遊戯王', 'モンハン')))
+
+@bot.command(aliases=['r', 'rl'])
+async def randlist(ctx):
+    # Googleスプレッドシート詠唱開始
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(GOOGLE_APPLICATION_CREDENTIALS, scope)
+    gc = gspread.authorize(credentials)
+    wks = gc.open('bot').sheet1
+
+    #  セルA1からA10の範囲の値を取ってきてリストに突っ込む
+    results = wks.range('A1:A10')
+    output = []
+    for result in results:
+        if result.value != '':
+            output.append(result.value)
+
+    # リストの中からランダムに一つピック
+    await ctx.send(random.choice(output))
 
 # Botの起動とDiscordサーバーへの接続
 bot.run(DISCORD_TOKEN)
